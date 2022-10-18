98adcb9311ffca6d9e88d24e53f0362c59f23d50
Annie24-ux <sibiyasa24@gmail.com>
2022-10-18 16:37:23 -0700

    T2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,8 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
 		_putchar('\n');
 	}
 }
