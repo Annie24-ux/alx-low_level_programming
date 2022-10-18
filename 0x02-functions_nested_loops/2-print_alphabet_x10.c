f96db9bf8aa33a4aa573e766a0a0847887c92d0d
Annie24-ux <sibiyasa24@gmail.com>
2022-10-18 16:13:01 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print alphabets in lowercase xby ten.
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char alphabet;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
+
