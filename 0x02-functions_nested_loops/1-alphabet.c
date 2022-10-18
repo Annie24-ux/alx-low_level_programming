dbfc7ca2099f66acea32a530abed235a4f73967f
Annie24-ux <sibiyasa24@gmail.com>
2022-10-18 15:42:56 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabets in lowercasse,then a new line follows.
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
