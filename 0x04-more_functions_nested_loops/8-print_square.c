46c44c99aedc53a85d47b023b7b478a466b3bd60
Annie24-ux <sibiyasa24@gmail.com>
2022-10-21 08:50:33 -0700

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square -  prints a square of size size, followed by a new line.
+ * @size: size of the squares to be checked
+ * Return: void
+ */
+
+void print_square(int size)
+
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i <= size; size++)
+	{
+		for (j = 0; j <= (size); size++)
+		{
+			_putchar("#");
+		}
+		_putchar('\n');
+	}
+}
+
+
+
+
