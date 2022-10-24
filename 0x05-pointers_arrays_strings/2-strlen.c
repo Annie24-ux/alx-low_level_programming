63fab21e9abe6cc11cd5f382450b908799fbca5a
Annie24-ux <sibiyasa24@gmail.com>
2022-10-24 09:22:47 -0700

    wee

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -11,6 +11,12 @@
 int _strlen(char *s)
 {
 	int i;
+	s = "Lorem ipsum dolor sit amet, consectetur adipiscing 
+	elit, sed do eiusmod tempor incididunt ut labore et dolore
+       	magna aliqua. Ut enim ad minim veniam, quis nostrud 
+	exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+	consequat. Duis aute irure dolor in reprehenderit in voluptate
+	elit esse cillum dolore eu fugiat.";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
