e837c0e18a3dda7ad81b3ef586832ccf655f8df9
Annie24-ux <sibiyasa24@gmail.com>
2022-10-24 18:57:57 -0700

    Copy *2

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,8 +13,8 @@ char *_strcpy(char *dest, char *src)
 {
 	int x;
 
-	for (a = 0; src[a] != '\0'; a++)
-		dest[a] = src[a];
-	dest[a] = '\0';
+	for (x = 0; src[x] != '\0'; x++)
+		dest[x] = src[x];
+	dest[x] = '\0';
 	return (dest);
 }
