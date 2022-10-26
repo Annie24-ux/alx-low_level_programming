dec48bcb61bc4c761d41af9f0e4eea912e3452fa
Annie24-ux <sibiyasa24@gmail.com>
2022-10-26 10:00:24 -0700

    Adding strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,11 +3,14 @@
 
 /**
  * *_strcat - a function that concatenates two strings
- *@*dest - dest string
- *@*src - src string 
+ *@dest:dest string
+ *@src: src string
  *Return: Always 0
  */
 
 char *_strcat(char *dest, char *src)
 {
+	_putchar("%s\n", strcat(dest, src));
+	return (0);
+}
 
