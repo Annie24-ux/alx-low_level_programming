443c9b170b64c0a6cf4fb0fb6a3cf980ef27e640
Annie24-ux <sibiyasa24@gmail.com>
2022-11-15 18:05:58 -0800

    Filename

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,13 +3,11 @@
 /**
  * main- a program that prints the name of the file it was compiled
  * from, followed by a new line.
- *@argc: number of arguments
- *@argv: a number of arguments
  * Return: Always (0)
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
-	_putchar("%s\n", argv[0]);
+	_putchar("%s\n", __FILE__);
 	return (0);
 }
