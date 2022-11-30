502a6942f8497bf39ac33b172324b89236846640
Annie24-ux <sibiyasa24@gmail.com>
2022-11-30 08:39:58 -0800

    Number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len-number of elements
+ * Return:number of node
+ * @h:next point of node
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+
+	}
+	return (count);
+}
