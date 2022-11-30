be24e4bb608ece4e35e58132a7775cf7b919b93f
Annie24-ux <sibiyasa24@gmail.com>
2022-11-30 09:17:54 -0800

    size of list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * add_nodeint-adds nodes to list
+ * @head:double head pointer to next node
+ *Return:the address of the new element, or NULL if it failed
+ *@n:integer
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *ptr;
+
+	if (head == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->n = n;
+	ptr->next = *head;
+	*head = ptr;
+
+	return (ptr);
+}
