8e4a15de487192a7fb87e33d554553a74711d7aa
Annie24-ux <sibiyasa24@gmail.com>
2022-11-30 05:40:44 -0800

    Printing elements of lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * print_listint-print elements of the lists
+ * @h:name of linked lists
+ *Return:number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != 0)
+	{
+		printf("%d\n", h->n);
+		counter++;
+
+		h = h->next
+	}
+	return (counter);
+}
